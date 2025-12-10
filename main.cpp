78011094d0319237b4666ec8ba87233c3553bb2a
paXen-code <vika.fir.2006@gmail.com>
2025-12-10 10:41:11 +0500

    каркас

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+struct shaurma
+{
+    int kalorii;
+    double ves;
+    wstring sous;
+    wstring meat;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    //функция 1:
+
+    //функция 2:
+
+    //функция 3:
+
+    return 0;
+}
