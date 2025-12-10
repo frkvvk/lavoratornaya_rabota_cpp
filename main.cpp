362fb7fee3a7b62e68f753f39901840643a5431d
PaVeLlLllLLlLLlLLLLlll <pavel.a.krut@gmail.com>
2025-12-10 10:53:35 +0500

    функция 3

--- main.cpp
+++ main.cpp
@@ -32,6 +32,10 @@ int main()
     wcout << ss.str();
     //функция 3:
     ofstream out("out.bin", ios::binary);
+    for(const auto &vid : vse_shaurmy)
+    {
+        out.write(reinterpret_cast<const char*>(&vid), sizeof(shaurma));
+    }
 
     return 0;
 }
